51db58cbd6138fb7c08166214339b315859f8135
Yash Raj <kyashraj991@gmail.com>
2025-04-09 10:10:17 +0530

    Schema setup

---
+++ src/model/User.ts
@@ -0,0 +1,66 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface Message extends Document {
+  content: string;
+  createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+
+  createdAt: {
+    type: Date,
+    required: true,
+    defualt: Date.now,
+  },
+});
+
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isVerified: boolean;
+  isAcceptingMessage: boolean;
+  messages: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, 'Username is required'],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, 'Verification code is required'],
+  },
+
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, 'Verification code expiry is required'],
+  },
+  isVerified: {
+    type: Boolean,
+    defualt: false,
+  },
+  isAcceptingMessage: {
+    type: Boolean,
+    defualt: true,
+  },
+
+  messages: [MessageSchema],
+});
