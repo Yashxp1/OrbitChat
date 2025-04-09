2e95f91c17ca941a55926e0db2e731fd3a24d370
Yash Raj <kyashraj991@gmail.com>
2025-04-09 12:52:56 +0530

    Email verification setup

--- src/helpers/sendVerificationEmail.tsx
+++ src/helpers/sendVerificationEmail.tsx
@@ -1,2 +1,27 @@
 import { resend } from '@/lib/resend';
+import VerificationEmail from '../../emails/VerificationEmail';
 
+import { ApiResponse } from '@/types/ApiResponse';
+
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<ApiResponse> {
+  try {
+    await resend.emails.send({
+      from: 'you@example.com',
+      to: email,
+      subject: 'Verification code',
+      react: VerificationEmail({ username, otp: verifyCode }),
+    });
+
+    return { success: true, message: 'Verification mail sent' };
+  } catch (error) {
+    console.log('Email error', error);
+    return {
+      success: false,
+      message: 'failed to send verication email',
+    };
+  }
+}
