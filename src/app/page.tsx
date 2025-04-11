eeb2ec053d687e4a5497f27a0a52f2f393bec1a5
Yash Raj <kyashraj991@gmail.com>
2025-04-11 16:36:03 +0530

    new start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,107 +1,103 @@
-import React, { useState } from 'react';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import { Badge, Book, Brain } from 'lucide-react';
-import { div, h2 } from 'framer-motion/m';
-import { span } from 'framer-motion/client';
+import Image from "next/image";
 
-const popularCat = [
-  'Science',
-  'History',
-  'Movies',
-  'Games',
-  'Politics',
-  'Tech',
-  'Math',
-  'Facts',
-];
-
-const faq = [
-  {
-    logo: <Book className='text-rose-500 text-2xl' />,
-    question: 'Vast Knowledge Base',
-    answer:
-      'Access thousands of quizzes across multiple categories updated regularly.',
-  },
-  {
-    logo: <Badge className='text-blue-500 text-2xl '/>,
-    question: 'Compete & Win',
-    answer:
-      'Challenge friends, join leaderboards, and earn achievements as you play.',
-  },
-  {
-    logo: <Brain className='text-yellow-500 text-2xl' />,
-    question: 'Learn As You Play',
-    answer:
-      'Detailed explanations and resources help you improve with every quiz.',
-  },
-];
-
-const Home = () => {
+export default function Home() {
   return (
-    <div className="">
-      <Navbar />
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2 tracking-[-.01em]">
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+              src/app/page.tsx
+            </code>
+            .
+          </li>
+          <li className="tracking-[-.01em]">
+            Save and see your changes instantly.
+          </li>
+        </ol>
 
-      <div className="flex flex-col py-12 mx-4 lg:mx-8">
-        <div className="">
-          <h1 className="text-purple-500 text-4xl md:text-5xl lg:text-6xl font-bold">
-            Challenge Your Knowledge with{' '}
-            <span className="text-white">zapIQ</span>
-          </h1>
-          <p className="py-7 text-lg font-extralight text-gray-400">
-            Test your skills, compete with friends, and learn something new with
-            our collection of thousands of quizzes across various categories.
-          </p>
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read our docs
+          </a>
         </div>
-
-        <div className="space-x-2">
-          <button className="px-6 py-3 rounded-md bg-purple-600">
-            Get started
-          </button>
-          <button className="px-6 py-3 rounded-md bg-gray-700">
-            Learn More
-          </button>
-        </div>
-        <div className="flex justify-center items-center my-5">
-          <div className="items-center border rounded-lg  border-gray-800 p-3 my-10 md:w-[70%] lg:w-[70%]">
-            <h1 className="text-2xl text-center md:text-3xl lg:text-3xl font-bold w-full py-3">
-              Popular categories
-            </h1>
-            <div className="grid grid-cols-2 gap-3 ">
-              {popularCat.map((cat, idx) => (
-                <div
-                  key={idx}
-                  className="border-1 text-center hover:bg-purple-600 hover:transition border-black bg-gray-700 rounded-lg px-3 py-2"
-                >
-                  {cat}
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="flex flex-col justify-center items-center border border-gray-700 my-3">
-        <h1 className="text-xl md:text-2xl lg:text-4xl font-extrabold py-12">
-          Why choose zapIQ?
-        </h1>
-
-        <div className="w-[80%] mb-12">
-          {faq.map((items, idx) => (
-            <div key={idx} className='bg-gray-800 border flex flex-col justify-center gap-4 h-36 border-gray-700 p-4 w-full  rounded-lg my-4' >
-              <span>{items.logo}</span>
-              <h2 className='text-xl'>{items.question}</h2>
-              <p className='text-md text-gray-300 '>{items.answer}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      <div>
-        <Footer />
-      </div>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
     </div>
   );
-};
-
-export default Home;
+}
