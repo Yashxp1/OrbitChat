2ba012019950a75958b9b7b0e7bc44d3c180cdad
Yash Raj <kyashraj991@gmail.com>
2025-04-11 19:53:07 +0530

    create video model and controller

--- models/User.ts
+++ models/User.ts
@@ -4,7 +4,7 @@ import bcrypt from 'bcrypt';
 export interface IUser {
   email: string;
   password: string;
-  _id: mongoose.Types.ObjectId;
+  _id?: mongoose.Types.ObjectId;
   createdAt?: Date;
   updatedAt?: Date;
 }
